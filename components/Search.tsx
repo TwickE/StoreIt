1e33cb7fc6131c344f3c0b14246f3762bc7caaad
TwickE <118013263+TwickE@users.noreply.github.com>
2025-01-21 16:30:49 +0000

    Finished the filter and search functionality

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,9 +3,13 @@
 import Image from "next/image"
 import { Input } from "@/components/ui/input"
 import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { Models } from "node-appwrite";
 import { getFiles } from "@/lib/actions/file.actions";
+import Thumbnail from "@/components/Thumbnail";
+import FormatedDateTime from "@/components/FormatedDateTime";
+import { useRouter } from "next/navigation";
+import { useDebounce } from 'use-debounce';
 
 
 const Search = () => {
@@ -14,23 +18,39 @@ const Search = () => {
     const searchQuery = searchParams.get("query") || "";
     const [results, setResults] = useState<Models.Document[]>([]);
     const [open, setOpen] = useState(false);
+    const router = useRouter();
+    const path = usePathname();
+    const [debouncedQuery] = useDebounce(query, 300);
 
     useEffect(() => {
         const fetchFiles = async () => {
-            const files = await getFiles({ searchText: query });
+            if(debouncedQuery.length === 0) {
+                setResults([]);
+                setOpen(false);
+                return router.push(path.replace(searchParams.toString(), ""));
+            }
+
+            const files = await getFiles({ types: [], searchText: debouncedQuery });
 
             setResults(files.documents);
             setOpen(true);
         }
 
         fetchFiles();
-    }, [query])
+    }, [debouncedQuery]);
 
     useEffect(() => {
         if (!searchQuery) {
             setQuery("");
         }
-    }, [searchQuery])
+    }, [searchQuery]);
+
+    const handleClickItem = (file: Models.Document) => {
+        setOpen(false);
+        setResults([]);
+
+        router.push(`${(file.type === "video" || file.type === "audio") ? "media" : file.type + "s"}?query=${query}`);
+    }
 
     return (
         <div className="search">
@@ -51,8 +71,17 @@ const Search = () => {
                     <ul className="search-result">
                         {results.length > 0 ? (
                             results.map((file) => (
-                                <li key={file.$id}>
-                                    {file.name}
+                                <li key={file.$id} className="flex items-center justify-between" onClick={() => handleClickItem(file)}>
+                                    <div className="flex cursor-pointer items-center gap-4">
+                                        <Thumbnail
+                                            type={file.type}
+                                            extension={file.extension}
+                                            url={file.url}
+                                            className="siz-9 min-w-9"
+                                        />
+                                        <p className="subtitle-2 line-clamp-1 text-light-100">{file.name}</p>
+                                    </div>
+                                    <FormatedDateTime date={file.$createdAt} className="caption line-clamp-1 text-light-200"/>
                                 </li>
                             ))
                         ) : (
